7a91fe79c65946253ca2636b3cd6ed66fedbc993
Andrew Chan <chan94306@gmail.com>
2016-01-20 19:43:45 -0800

    Parse initialization complete

---
+++ app/src/main/java/droidsquad/voyage/model/ParseModel.java
@@ -0,0 +1,24 @@
+package droidsquad.voyage.model;
+
+import android.content.Context;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Andrew on 1/20/16.
+ */
+public class ParseModel {
+
+    public static final String USER_OBJECT = "User";
+    public static final String TRIP_OBJECT = "Trip";
+    // add more key/value final Strings
+
+
+    private Context context;
+
+    public ParseModel(Context context) {
+        // Parse.enableLocalDatastore(context);
+        // Parse.initialize(context);
+    }
+}
