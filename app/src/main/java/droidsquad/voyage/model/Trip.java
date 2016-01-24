2e3768fd62e492bd93825e561712e98dba7d00c7
Gustavo Umbelino <gumbelin@ucsd.edu>
2016-01-23 21:53:01 -0800

    test

--- app/src/main/java/droidsquad/voyage/model/Trip.java
+++ app/src/main/java/droidsquad/voyage/model/Trip.java
@@ -1,7 +1,27 @@
 package droidsquad.voyage.model;
 
+import java.util.Date;
+
 /**
  * Created by Andrew on 1/23/16.
  */
 public class Trip {
+
+    // General
+    private String tripName;
+    private int memberLimit;
+    boolean privateTrip;
+
+    // Details
+    private String leavingFrom;
+    private String destination;
+    private Date dateFrom;
+    private Date dateTo;
+    private String transportation;
+
+    //TODO: Define minimum requirements to create a trip
+    public Trip( String name, int limint, boolean priv, String from, String to ){
+
+    }
+
 }
