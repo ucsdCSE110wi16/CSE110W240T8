00d410c26310678397c8f9517191689edeadb2fe
Gustavo Umbelino <gumbelin@ucsd.edu>
2016-01-24 00:08:05 -0800

    Working on save trip data to Parse

--- app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
+++ app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
@@ -2,6 +2,10 @@ package droidsquad.voyage.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import droidsquad.voyage.R;
 import droidsquad.voyage.controller.CreateTripController;
@@ -19,5 +23,27 @@ public class CreateTripActivity extends AppCompatActivity {
 
         this.getApplication();
 
+    }
+
+    private static boolean checkDates( DatePicker from, DatePicker to ) {
+
+        Date dateFrom = getDateFromDatePicker(from);
+        Date dateTo = getDateFromDatePicker(to);
+
+        return dateFrom.before(dateTo);
+    }
+
+    private static Date getDateFromDatePicker(DatePicker datePicker){
+
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        System.out.println("Date: " + calendar.getTime());
+
+        return calendar.getTime();
     }
 }
--- app/src/main/java/droidsquad/voyage/model/Trip.java
+++ app/src/main/java/droidsquad/voyage/model/Trip.java
@@ -1,5 +1,8 @@
 package droidsquad.voyage.model;
 
+import com.parse.ParseObject;
+
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -7,21 +10,79 @@ import java.util.Date;
  */
 public class Trip {
 
+    private static final int DEFAULT_LIMIT = 5;
+    private static final String DEFAULT_TRANSP = "Plane";
+
     // General
     private String tripName;
-    private int memberLimit;
-    boolean privateTrip;
+    private String tripOrigin;
+    private String tripDestination;
+    boolean tripPrivate;
 
     // Details
-    private String leavingFrom;
-    private String destination;
+    private int tripLimit;
     private Date dateFrom;
     private Date dateTo;
     private String transportation;
 
-    //TODO: Define minimum requirements to create a trip
-    public Trip( String name, int limint, boolean priv, String from, String to ){
+    public Trip( String name, String orig, String dest, boolean priv,
+                 int limit, Date dateFrom, Date dateTo, String transp ){
 
+        tripName = name;
+        tripOrigin = orig;
+        tripDestination = dest;
+        tripPrivate = priv;
+
+        tripLimit = limit;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+        transportation = transp;
+
+    }
+
+    // TODO: Define minimum requirements to create a trip
+    public Trip( String name, String orig, String dest, boolean priv  ){
+        tripName = name;
+        tripOrigin = orig;
+        tripDestination = dest;
+        tripPrivate = priv;
+
+        // TODO: Define default values
+        tripLimit = DEFAULT_LIMIT;
+        Calendar calendar = Calendar.getInstance();
+        dateFrom = calendar.getTime();
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH));
+        dateTo = calendar.getTime();
+        transportation = DEFAULT_TRANSP;
+    }
+
+    public void setLimit( int limit ) {
+        tripLimit = limit;
+    }
+
+    public void setDateFrom( Date from ) {
+        dateFrom = from;
+    }
+
+    public void setDateto( Date to ) {
+        dateTo = to;
+    }
+
+    public void setTrasp( String transp ){
+        transportation = transp;
+    }
+
+    public void save() {
+        ParseObject trip = new ParseObject("Trip");
+        trip.put("name", tripName);
+        trip.put("origin", tripOrigin);
+        trip.put("destination", tripDestination);
+        trip.put("private", tripPrivate);
+        trip.put("dateFrom", dateFrom);
+        trip.put("dateTo", dateTo);
+        trip.put("limit", tripLimit);
+        trip.put("transportation", transportation);
+        trip.saveInBackground();
     }
 
 }
