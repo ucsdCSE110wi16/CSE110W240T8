9a8d45ea4b4604f4812b52bb909821c3009c7eef
Heitor Schueroff <h.schueroff@gmail.com>
2016-01-24 18:36:15 -0800

    Create Trip Layout v1.0

--- app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
+++ app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
@@ -14,10 +14,5 @@ public class CreateTripActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
-
-        controller = new CreateTripController(this);
-
-        this.getApplication();
-
     }
 }
