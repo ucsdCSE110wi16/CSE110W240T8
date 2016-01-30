a5751bb99ac85bb0d1be22f83c6f379fe4752cde
Heitor Schueroff <h.schueroff@gmail.com>
2016-01-30 07:51:26 -0800

    Create Trip Material Design and Date Checks

--- app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
+++ app/src/main/java/droidsquad/voyage/activity/CreateTripActivity.java
@@ -1,43 +1,50 @@
 package droidsquad.voyage.activity;
 
 import android.app.DatePickerDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 import droidsquad.voyage.R;
 import droidsquad.voyage.controller.CreateTripController;
-import droidsquad.voyage.model.Trip;
-
 
 public class CreateTripActivity extends AppCompatActivity {
     private CreateTripController controller;
 
+    private TextInputLayout mMemberLimitWrapper;
+    private TextInputLayout mLeavingFromWrapper;
+    private TextInputLayout mDestinationWrapper;
+    private TextInputLayout mDateFromWrapper;
+    private TextInputLayout mDateToWrapper;
+
     private EditText mTripNameView;
-    private EditText mMemberLimitView;
+    private EditText mDateFromView;
+    private EditText mDateToView;
+
+    private TextView mPrivateHelpView;
+
     private CheckBox mPrivateView;
+    private Spinner mTransportation;
 
     private AutoCompleteTextView mLeavingFromView;
     private AutoCompleteTextView mDestinationView;
 
-    private Spinner mTransportation;
-    private TextView mDateFromView;
-    private TextView mDateToView;
     private Calendar calendarFrom;
     private Calendar calendarTo;
 
+    private long minDateAllowed;
     private static final int DEFAULT_TRIP_LENGTH = 7;
     private static final String TAG = CreateTripActivity.class.getSimpleName();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM dd", Locale.US);
@@ -51,29 +58,53 @@ public class CreateTripActivity extends AppCompatActivity {
 
         // Set up the trip form.
         mTripNameView = (EditText) findViewById(R.id.trip_name);
-        mMemberLimitView = (EditText) findViewById(R.id.member_limit);
         mPrivateView = (CheckBox) findViewById(R.id.private_check);
         mTransportation = (Spinner) findViewById(R.id.transportation);
-        mDateFromView = (TextView) findViewById(R.id.date_from);
-        mDateToView = (TextView) findViewById(R.id.date_to);
         mLeavingFromView = (AutoCompleteTextView) findViewById(R.id.leaving_from);
         mDestinationView = (AutoCompleteTextView) findViewById(R.id.destination);
+
+        mPrivateHelpView = (TextView) findViewById(R.id.trip_private_help);
+
+        mMemberLimitWrapper = (TextInputLayout) findViewById(R.id.member_limit_wrapper);
+        mLeavingFromWrapper = (TextInputLayout) findViewById(R.id.leaving_from_wrapper);
+        mDestinationWrapper = (TextInputLayout) findViewById(R.id.destination_wrapper);
+        mDateFromWrapper = (TextInputLayout) findViewById(R.id.date_from_wrapper);
+        mDateToWrapper = (TextInputLayout) findViewById(R.id.date_to_wrapper);
+
+        mDateFromView = mDateFromWrapper.getEditText();
+        mDateToView = mDateToWrapper.getEditText();
 
         controller.setUpPlacesAutofill(mLeavingFromView, 0);
         controller.setUpPlacesAutofill(mDestinationView, 1);
 
-        // Spinner Drop down elements
-        String categories[] = {"Plane", "Metro", "Bus", "Car"};
+        // Set Checkbox Listener for changing Help Text
+        mPrivateView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPrivateHelpView.setText(
+                        (mPrivateView.isChecked())
+                                 ? R.string.help_trip_private
+                                 : R.string.help_trip_public
+                );
+            }
+        });
 
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, categories);
+        // Set up Date Picker listeners
+        mDateFromView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minDateAllowed = System.currentTimeMillis() - 1000;
+                setFromDate(v);
+            }
+        });
 
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        mTransportation.setAdapter(dataAdapter);
+        mDateToView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minDateAllowed = calendarFrom.getTimeInMillis() - 1000;
+                setToDate(v);
+            }
+        });
 
         // Set up default dates
         calendarFrom = Calendar.getInstance();
@@ -81,20 +112,34 @@ public class CreateTripActivity extends AppCompatActivity {
         calendarTo.add(Calendar.DAY_OF_WEEK, DEFAULT_TRIP_LENGTH);
         mDateFromView.setText(dateFormat.format(calendarFrom.getTime()));
         mDateToView.setText(dateFormat.format(calendarTo.getTime()));
+
+        // Set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.trip_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
-    public void showFromDateDialog(View view) {
-        showDateDialogAndUpdateView(new DatePickerDialog.OnDateSetListener() {
+    public void setFromDate(View view) {
+        showDateDialog(new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                long previousDateTime = calendarFrom.getTimeInMillis();
                 calendarFrom.set(year, monthOfYear, dayOfMonth);
+
+                // Compute difference and adjust the toDate accordingly
+                long diff = calendarFrom.getTimeInMillis() - previousDateTime;
+                calendarTo.setTimeInMillis(calendarTo.getTimeInMillis() + diff);
+
+                mDateToView.setText(dateFormat.format(calendarTo.getTime()));
                 mDateFromView.setText(dateFormat.format(calendarFrom.getTime()));
             }
         }, calendarFrom);
     }
 
-    public void showToDateDialog(View view) {
-        showDateDialogAndUpdateView(new DatePickerDialog.OnDateSetListener() {
+    public void setToDate(View view) {
+        showDateDialog(new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 calendarTo.set(year, monthOfYear, dayOfMonth);
@@ -103,11 +148,12 @@ public class CreateTripActivity extends AppCompatActivity {
         }, calendarTo);
     }
 
-    public void showDateDialogAndUpdateView(DatePickerDialog.OnDateSetListener listener, Calendar cal) {
+    public void showDateDialog(DatePickerDialog.OnDateSetListener listener, Calendar cal) {
         DatePickerDialog datePickerDialog = new DatePickerDialog(this, listener,
                 cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)
         );
 
+        datePickerDialog.getDatePicker().setMinDate(minDateAllowed);
         datePickerDialog.show();
     }
 
@@ -119,33 +165,12 @@ public class CreateTripActivity extends AppCompatActivity {
     public void attemptCreateTrip(View view) {
         String tripName = mTripNameView.getText().toString();
 
-        int memberLimit = 0;
-        if (!isEmpty(mMemberLimitView)) {
-            memberLimit = Integer.parseInt(mMemberLimitView.getText().toString());
+        if (tripName.length() < 3) {
+            mTripNameView.setError("Trip name must be at least 3 characters long.");
         }
-
-        boolean privateTrip = mPrivateView.isChecked();
-
-        String leavingFrom = mLeavingFromView.getText().toString();
-        String destination = mDestinationView.getText().toString();
-
-        Date dateFrom = calendarFrom.getTime();
-        Date dateTo = calendarTo.getTime();
-
-        String transportation = mTransportation.getSelectedItem().toString();
-
-        // TODO: CHANGE HERE
-        Trip newTrip = new Trip(tripName, leavingFrom, destination, privateTrip,
-                memberLimit, dateFrom, dateTo, transportation);
-        newTrip.save();
     }
 
     private boolean isEmpty(EditText etText) {
         return etText.getText().toString().trim().length() <= 0;
-    }
-
-    private static boolean checkDates(DatePicker from, DatePicker to) {
-        // TODO: Move to Controller and implement
-        return false;
     }
 }
