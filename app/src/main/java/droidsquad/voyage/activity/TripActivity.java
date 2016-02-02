43639942585c1aa24a9c87ceb24ed79d57a99553
Heitor Schueroff <h.schueroff@gmail.com>
2016-02-02 11:30:21 -0800

    Initial tripActivity setup

---
+++ app/src/main/java/droidsquad/voyage/activity/TripActivity.java
@@ -0,0 +1,34 @@
+package droidsquad.voyage.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+import droidsquad.voyage.R;
+
+public class TripActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_trip);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.trip_collapsing_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.trip_toolbar);
+
+        collapsingToolbar.setTitle("Backpacking in Yosemite");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.trip_toolbar, menu);
+        return true;
+    }
+}
