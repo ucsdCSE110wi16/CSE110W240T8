9add3a91b77746e99d172b68230903c56a8b63d3
Heitor Schueroff <h.schueroff@gmail.com>
2016-02-01 23:15:46 -0800

    TripCardAdapter first implementation

--- app/src/main/java/droidsquad/voyage/model/TripCardAdapter.java
+++ app/src/main/java/droidsquad/voyage/model/TripCardAdapter.java
@@ -4,38 +4,34 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import droidsquad.voyage.R;
 
-/**
- * Created by Andrew on 1/31/16.
- */
 public class TripCardAdapter extends RecyclerView.Adapter<TripCardAdapter.ViewHolder> {
     private ArrayList<Trip> trips = new ArrayList<>();
 
-    // TODO:
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mName;
+        public TextView mCities;
+        public TextView mDates;
+        public ImageView mPrivateIcon;
 
-        // example:
-        // public TextView title;
-        // public
-
-        // TODO: i'm not actually sure if you pass it a view or not, double check/research this
         public ViewHolder(View view) {
             super(view);
-            // TODO: UI references here
-            // example:
-            // title = (TextView) findViewById(R.id.title...);
+
+            mName = (TextView) view.findViewById(R.id.trip_card_name);
+            mCities = (TextView) view.findViewById(R.id.trip_card_cities);
+            mDates = (TextView) view.findViewById(R.id.trip_card_date_range);
+            mPrivateIcon = (ImageView) view.findViewById(R.id.trip_card_is_private);
         }
     }
 
     public TripCardAdapter() {
-        trips = new ArrayList<>();
+        this.trips = new ArrayList<>();
     }
 
     public void updateData(ArrayList<Trip> trips) {
@@ -44,28 +40,23 @@ public class TripCardAdapter extends RecyclerView.Adapter<TripCardAdapter.ViewHo
 
     // Create new views (invoked by the layout manager)
     @Override
-    public TripCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
+    public TripCardAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.trip_card, parent, false);
 
-        // set the view's size, margins, paddings and layout parameters
-
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        return new ViewHolder(view);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
+        Trip trip = this.trips.get(position);
 
-        // TODO: This is where you will bind all the data to the UI elements
-        // example:
-        // holder.mTextView.setText(trips.get(position).getOrigin());
-
+        holder.mName.setText(trip.getName());
+        holder.mCities.setText(trip.getOrigin() + " –> " + trip.getDestination());
+        holder.mDates.setText(trip.getDateFrom() + " – " + trip.getDateTo());
+        holder.mPrivateIcon.setVisibility(
+                (trip.isPrivate()) ? View.VISIBLE : View.GONE);
     }
 
     // Return the size of your dataset (invoked by the layout manager)
