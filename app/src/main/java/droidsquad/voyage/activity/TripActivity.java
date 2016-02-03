d999fdfc9a88d87ad33a41b5722b3b76679c6283
Heitor Schueroff <h.schueroff@gmail.com>
2016-02-02 22:05:31 -0800

    Trip Layout and Activity Setup

--- app/src/main/java/droidsquad/voyage/activity/TripActivity.java
+++ app/src/main/java/droidsquad/voyage/activity/TripActivity.java
@@ -1,34 +1,148 @@
 package droidsquad.voyage.activity;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import droidsquad.voyage.R;
 
 public class TripActivity extends AppCompatActivity {
+    private CollapsingToolbarLayout mCollapsingToolbar;
+    private FloatingActionButton mFAB;
+    private ImageView mHeaderImageView;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_trip);
 
-        CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) findViewById(R.id.trip_collapsing_toolbar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.trip_toolbar);
-
-        collapsingToolbar.setTitle("Backpacking in Yosemite");
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initUI();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.trip_toolbar, menu);
+        getMenuInflater().inflate(R.menu.trip_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.trip_action_share:
+                startShareIntent();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * Initialize all the UI elements of this Activity
+     */
+    private void initUI() {
+        mCollapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        mHeaderImageView = (ImageView) findViewById(R.id.header_image);
+        mFAB = (FloatingActionButton) findViewById(R.id.fab);
+
+        // Set up toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mCollapsingToolbar.setTitle("Backpacking in Arizona");
+        mCollapsingToolbar.setExpandedTitleTypeface(Typeface.create("sans-serif", Typeface.BOLD));
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        setColors();
+    }
+
+    /**
+     * Set the colors of the toolbar and FAB based on the header image
+     */
+    private void setColors() {
+        Bitmap bitmap = ((BitmapDrawable) mHeaderImageView.getDrawable()).getBitmap();
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                int colorPrimary = ContextCompat.getColor(
+                        getApplicationContext(), R.color.colorPrimary);
+                int colorPrimaryDark = ContextCompat.getColor(
+                        getApplicationContext(), R.color.colorPrimaryDark);
+                int colorAccent = ContextCompat.getColor(
+                        getApplicationContext(), R.color.colorAccent);
+
+                Palette.Swatch mutedSwatch = palette.getMutedSwatch();
+                int vibrantColor = palette.getVibrantColor(colorAccent);
+
+                // Status bar and toolbar color
+                mCollapsingToolbar.setContentScrimColor(
+                        (mutedSwatch != null) ? mutedSwatch.getRgb() : colorPrimary);
+                mCollapsingToolbar.setExpandedTitleColor(
+                        (mutedSwatch != null) ? mutedSwatch.getBodyTextColor() : 0xffffff);
+                setStatusBarColor(
+                        (mutedSwatch != null) ? darkenColor(mutedSwatch.getRgb()) : colorPrimaryDark);
+
+                // Floating Action Button color
+                mFAB.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
+            }
+        });
+    }
+
+    /**
+     * Set the color of the status bar
+     *
+     * @param color Color to be set
+     */
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setStatusBarColor(int color) {
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(color);
+    }
+
+    /**
+     * Get a darker version of a color
+     *
+     * @param color Color to be darkened
+     * @return Darkened color
+     */
+    private int darkenColor(int color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(color, hsv);
+        hsv[2] *= 0.8f;
+        return Color.HSVToColor(hsv);
+    }
+
+    /**
+     * Displays a Chooser menu for sharing content
+     *
+     * TODO: Set up the content to be shared here
+     */
+    private void startShareIntent() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "Share"));
     }
 }
