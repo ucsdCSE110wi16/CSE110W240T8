ad4977eecc1ab063c652812ad5cca98e8bf1fef2
Heitor Schueroff <h.schueroff@gmail.com>
2016-02-10 12:00:29 -0800

    Documented ParseTripModel

--- app/src/main/java/droidsquad/voyage/model/ParseTripModel.java
+++ app/src/main/java/droidsquad/voyage/model/ParseTripModel.java
@@ -1,6 +1,5 @@
 package droidsquad.voyage.model;
 
-import android.content.Context;
 import android.util.Log;
 
 import com.parse.FindCallback;
@@ -11,21 +10,21 @@ import com.parse.ParseRelation;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import droidsquad.voyage.controller.TripListController;
 
 public class ParseTripModel {
     public static final String USER_OBJECT = "User";
     public static final String TRIP_OBJECT = "Trip";
     private static final String TAG = ParseUser.class.getSimpleName();
-    // add more key/value final Strings
+    // TODO: add more key/value final Strings
 
+    /**
+     * Save this trip to the database
+     *
+     * @param trip Trip to be saved
+     */
     public static void saveTrip(Trip trip) {
         Log.d(TAG, "Attempting to save trip to parse.\nTrip: " + trip.toString());
         ParseObject parseTrip = new ParseObject("Trip");
@@ -44,7 +43,7 @@ public class ParseTripModel {
         parseTrip.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
-                if(e == null){
+                if (e == null) {
                     Log.d(TAG, "SUCCESS");
                 } else {
                     Log.d(TAG, "FAILED");
@@ -55,6 +54,11 @@ public class ParseTripModel {
         trip.setTripId(parseTrip.getObjectId());
     }
 
+    /**
+     * Gets all the trips from the database that this user is part of
+     *
+     * @param callback Called with the trips as an argument
+     */
     public static void searchForAllTrips(final ParseTripCallback callback) {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Trip");
         query.whereEqualTo("creatorId", ParseUser.getCurrentUser().getObjectId());
@@ -67,12 +71,18 @@ public class ParseTripModel {
         });
     }
 
+    /**
+     * Helper method for getting all the trips from the database
+     *
+     * @param trips List to put the trips inside
+     * @param callback Called when all trips are retrieved from the database
+     */
     private static void getAllMyTrips(List<ParseObject> trips, ParseTripCallback callback) {
         ArrayList<Trip> allMyTrips = new ArrayList<>();
-        if(trips == null)
+        if (trips == null)
             return;
 
-        for(ParseObject parseTrip: trips) {
+        for (ParseObject parseTrip : trips) {
             String name = parseTrip.getString("name");
             String creatorId = parseTrip.getString("creatorId");
 
@@ -98,6 +108,9 @@ public class ParseTripModel {
         return ParseUser.getCurrentUser().getObjectId();
     }
 
+    /**
+     * Interface to implement a callback that is accepted in methods of this class
+     */
     public interface ParseTripCallback {
         void onCompleted(ArrayList<Trip> trip);
     }
