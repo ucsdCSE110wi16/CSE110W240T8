eccf55c1d36efc8b824b5689c6bbb7c13cb0ea23
Gustavo Umbelino <gumbelin@ucsd.edu>
2016-03-04 20:23:17 -0800

    Working on CreateTrip espresso tests

--- app/src/androidTest/java/droidsquad/voyage/CreateTripTest.java
+++ app/src/androidTest/java/droidsquad/voyage/CreateTripTest.java
@@ -1,8 +1,24 @@
 package droidsquad.voyage;
-//import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -14,30 +30,64 @@ import droidsquad.voyage.model.objects.Trip;
 /**
  * Created by gumbe on 1/23/2016.
  */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class CreateTripTest {
 
-    CreateTripActivity tripActivity;
-    Trip completeTrip;
-    Trip simpleTrip;
+    @Rule
+    public ActivityTestRule<CreateTripActivity> mCreateTripActivityRule = new ActivityTestRule(CreateTripActivity.class);
+
+    String TRIP_NAME;
+    String LEAVING_FROM;
 
     @Before
     public void setUp() throws Exception {
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2015, 05, 10);
-        Date from = calendar.getTime();
-        calendar.set(2015, 05, 17);
-        Date to = calendar.getTime();
+        TRIP_NAME = "TEST";
+        LEAVING_FROM = "San Diego, CA, USA";
 
-       // completeTrip = new Trip( "Complete", "La Jolla", "San Diego", false, 5,
-         //                                                       from, to, "Car" );
-       // simpleTrip = new Trip( "Simple", "Curitiba", "La Jolla", true );
     }
 
     @Test
-    public void save(){
-        ParseTripModel.saveNewTrip(completeTrip);
-        ParseTripModel.saveNewTrip(simpleTrip);
+    public void testTripNameChangeText() {
+
+        // Type trip name
+        onView(withId(R.id.trip_name))
+                .perform(typeText(TRIP_NAME), closeSoftKeyboard());
+
+        // Check that the text was changed.
+        onView(withId(R.id.trip_name)).check(matches(withText(TRIP_NAME)));
+
+    }
+
+    @Test
+    public void testPrivateCheckboxChangeText() {
+
+        // Check original text.
+        onView(withId(R.id.trip_private_help)).check(matches(withText(R.string.help_trip_private)));
+
+        // Un-check private checkbox
+        onView(withId(R.id.private_check))
+                .perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.trip_private_help)).check(matches(withText(R.string.help_trip_public)));
+
+    }
+
+    @Test
+    public void testLeavingFromChangeText() {
+
+        // Un-check private checkbox
+        onView(withId(R.id.leaving_from))
+                .perform(click());
+
+        /** TODO: Continue here */
+
+        // Check that the text was changed.
+        //onView(withId(R.id.trip_private_help)).check(matches(withText(R.string.help_trip_public)));
+
     }
 
 }
+
