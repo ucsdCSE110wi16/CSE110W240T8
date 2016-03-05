ebc0ded5c4413c5997103c4c1830a3fbd0a866b2
Zemei Zeng <zemeizeng@gmail.com>
2016-03-05 12:23:59 -0800

    Added Espresso tests to CreateTrip (transportation & name error checking)

--- app/src/androidTest/java/droidsquad/voyage/CreateTripTest.java
+++ app/src/androidTest/java/droidsquad/voyage/CreateTripTest.java
@@ -2,6 +2,7 @@ package droidsquad.voyage;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -12,8 +13,13 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isNotChecked;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
@@ -45,31 +51,43 @@ public class CreateTripTest {
     @Rule
     public ActivityTestRule<CreateTripActivity> mCreateTripActivityRule = new ActivityTestRule(CreateTripActivity.class);
 
+    String TRIP_NAME_TOO_SHORT;
     String TRIP_NAME;
     String LEAVING_FROM;
     String LOCATION_ERROR;
     String TRIP_NAME_ERROR;
+    String TRIP_TRANSPORTATION;
 
     @Before
     public void setUp() throws Exception {
 
-        TRIP_NAME = "TEST";
+        TRIP_NAME_TOO_SHORT = "AA";
+        TRIP_NAME = "123";
         LEAVING_FROM = "San Diego, CA, USA";
         LOCATION_ERROR = "Please enter a valid location";
         TRIP_NAME_ERROR = "Name must be at least 3 character long";
-
+        TRIP_TRANSPORTATION = "Plane";
     }
 
     @Test
     public void testTripNameChangeText() {
 
+        // check that trip name must be set
+        onView(withId(R.id.create_trip_button)).perform(click());
+        onView(withId(R.id.trip_name)).check(matches(hasErrorText(TRIP_NAME_ERROR)));
+
+        // check entering a trip name that is too short
+        onView(withId(R.id.trip_name)).perform(typeText(TRIP_NAME_TOO_SHORT));
+        onView(withId(R.id.trip_name)).check(matches(withText(TRIP_NAME_TOO_SHORT)));
+        onView(withId(R.id.create_trip_button)).perform(click());
+        onView(withId(R.id.trip_name)).check(matches(hasErrorText(TRIP_NAME_ERROR)));
+
         // Type trip name
         onView(withId(R.id.trip_name))
-                .perform(typeText(TRIP_NAME), closeSoftKeyboard());
+                .perform(clearText(), typeText(TRIP_NAME), closeSoftKeyboard());
 
         // Check that the text was changed.
         onView(withId(R.id.trip_name)).check(matches(withText(TRIP_NAME)));
-
     }
 
     @Test
@@ -126,9 +144,34 @@ public class CreateTripTest {
         // Check that the text was changed.
         //onView(withId(R.id.trip_private_help)).check(matches(withText(R.string.help_trip_public)));
 
+        //onView(withId(R.id.leaving_from)).perform(click());
+
+        // other possible ideas:
+
+        //createTripActivityRule.getActivity().onActivityResult
+        /*try {
+            Thread.sleep(5000); // give the app extra time to update
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }*/
+
+        //onView(withId(R.id.place_autocomplete_search_input))
+        //        .inRoot(withDecorView(not(is(createTripActivityRule.getActivity().getWindow().getDecorView()))))
+        //        .perform(typeText(tripOrigin)); <- doesn't work for some reason
+
+        //onView(withId(android.R.layout.simple_list_item_1)).perform(click()); <- select a trip from menu
+        //onView(withId(R.id.create_trip_button)).perform(click());
+        //onView(withId(R.id.destination)).check(matches(hasErrorText(tripLocationError)));
+
     }
 
-
-
+    @Test
+    public void testTransportation() {
+        // Select Plane transportation option and check that view updates
+        onView(withId(R.id.transportation)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(TRIP_TRANSPORTATION))).perform(click());
+        onView(withId(R.id.transportation)).check(matches(withSpinnerText(containsString(TRIP_TRANSPORTATION))));
+    }
 }
 
